a1cbc0c8304b94f5773650524257230c4aab4713
masakazu268 <dadofakane@gmail.com>
2025-09-09 20:37:42 +0900

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -6,8 +6,8 @@ st.caption("This is a simple Streamlit app.")
 
 
 st.subheader("自己紹介")
-st.text("私はChatGPT、OpenAIが開発したAI言語モデルです。")
+st.text("私は、まさかず。python独学中の60歳代です。")
 
-# 画像
+# 画git add .像
 image = Image.open("./data/01.jpg")
 st.image(image,width=300)
--- pages/page_1.py
+++ pages/page_1.py
@@ -3,5 +3,6 @@ import streamlit as st
 code = '''
 import streamlit as st
 st.title("Hello, Streamlit!")
+python
 '''
 st.code(code, language='python')
